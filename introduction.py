e7b0a63ae143d752862c7de95bb8856a03d2b039
frozen2938 <frozenforel@gmail.com>
2025-02-03 13:14:06 +0300

    my commit

---
+++ introduction.py
@@ -0,0 +1,7 @@
+
+#print is a method used to display output
+
+print("Frozen")
+print("eMobilis")
+print(39)
+print(100*2)
---
+++ valuable.py
@@ -0,0 +1,23 @@
+#Variable is a container used to store a value
+
+
+number = 10
+firstname = "Geoffrey"
+lastname = "Mwaniki"
+language = "python"
+a = 10
+b = 15
+c = 25
+
+print(number)
+print(firstname)
+print(lastname)
+print(language,"is a web programming language")
+
+print(firstname,lastname)
+print(a + b)
+
+
+#The sum of 10 and 15 is 25
+
+print("The sum of", a, "and", b, "is", c)
