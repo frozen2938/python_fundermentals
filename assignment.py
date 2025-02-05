50738b0a56fcb1153089a68df2c519c4dee1a30e
frozen2938 <frozenforel@gmail.com>
2025-02-05 22:24:00 +0300

    i commit

--- assignment.py
+++ assignment.py
@@ -1,7 +1,45 @@
 #1. A program to check weather a year is a leap year or not
+import re
+
+n = 2024
+x = 2025
+
+#example one
+if n % 4 == 0:
+    print(n, "is a leap year")
+else:
+    print(n, "is not a leap year")
+
+#example two
+if x % 4 == 0:
+    print(x, "is a leap year")
+else:
+    print(x, "is not a leap year")
+
+
+
 
 
 
 
 
 #2. A program to check weather a letter is a consonant or a vowel
+
+#reference
+letter = "E"
+mine = "z"
+
+#example one
+if re.match(r'[aeiouAEIOU]', letter):
+    print("The Letter is a Vowel")
+else:
+    print("The Letter is a Consonant")
+
+# example two
+if re.match(r'[aeiouAEIOU]', mine):
+     print("The Letter is a Vowel")
+else:
+     print("The Letter is a Consonant")
+
+
+
---
+++ extra.py
@@ -0,0 +1,6 @@
+if n%4==0 and n%100!=0:
+        if n%400==0:
+            print(n, "is a leap year.")
+    elif n%4!=0:
+        print(n, "is not a leap year.")
+
